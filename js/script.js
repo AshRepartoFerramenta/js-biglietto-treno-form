44e8bcb180ba5739df1fe5b9d5a9ac34f6e8c71f
AshRepartoFerramenta <dcapod07@hotmail.com>
2020-05-26 07:43:12 +0200

    Restart and change

---
+++ js/script.js
@@ -0,0 +1,62 @@
+// Variabile e funzione Bottoni
+var buttonGenera = document.getElementById('genera');
+buttonGenera.addEventListener('click',
+  function() {
+    var inputNome = document.getElementById('nome-utente');
+    var nomeUtente = inputNome.value;
+    console.log(nomeUtente);
+
+    var inputKm = document.getElementById('km');
+    var kmDaPercorrere = parseInt(inputKm.value);
+    console.log(kmDaPercorrere);
+
+    var selectFasciaEta = document.getElementById('fascia-età');
+    var fasciaEtà = selectFasciaEta.value;
+    console.log(fasciaEtà);
+// Variabile Calcolo Ticket
+    var ticketPriceForKm = 0.21;
+    var ticketPrice = ticketPriceForKm * kmDaPercorrere;
+    console.log(ticketPrice);
+// Variabile con Condizione e Sconto  rilascio tariffa
+    var offerta = 'Tariffa Standard';
+
+    if (fasciaEtà == 'minorenne') {
+      ticketPrice = ticketPrice - ((ticketPrice * 20) / 100);
+      offerta = 'Sconto Minorenne';
+      console.log(ticketPrice);
+    } else if (fasciaEtà == 'over65') {
+      ticketPrice = ticketPrice - ((ticketPrice * 40) / 100);
+      offerta = 'Sconto Over 65';
+      console.log(ticketPrice);
+    }
+// Calcolo di riferimento form rilascio biglietto
+    document.getElementById('offerta').innerHTML = offerta;
+    document.getElementById('nome-passeggero').innerHTML = nomeUtente;
+    document.getElementById('carrozza').innerHTML = Math.floor(Math.random() * 9) + 1;
+    document.getElementById('codice-cp').innerHTML = Math.floor(Math.random() * (100000 - 90000 + 1) ) + 90000;
+    document.getElementById('prezzo').innerHTML = ticketPrice.toFixed(2);
+
+    var ticketDiv = document.getElementById('ticket');
+    ticketDiv.classList.remove('hidden');
+    ticketDiv.classList.add('show');
+  }
+);
+// Associazione di validità emissione 
+  var buttonAnnulla = document.getElementById('annulla');
+  buttonAnnulla.addEventListener('click',
+    function() {
+      document.getElementById('nome-passeggero').innerHTML = '';
+      document.getElementById('offerta').innerHTML = '';
+      document.getElementById('carrozza').innerHTML = '';
+      document.getElementById('codice-cp').innerHTML = '';
+      document.getElementById('prezzo').innerHTML = '';
+
+      document.getElementById('nome-utente').value = '';
+      document.getElementById('km').value = '';
+      document.getElementById('fascia-età').value = '';
+
+      var ticketDiv = document.getElementById('ticket');
+      ticketDiv.classList.add('hidden');
+      ticketDiv.classList.remove('show');
+    }
+  );
